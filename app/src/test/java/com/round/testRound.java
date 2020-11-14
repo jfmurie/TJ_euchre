defcc0d9dc02befef8bd9b543fb2787ffd975e91
ljjohnst <55628783+ljjohnst@users.noreply.github.com>
2020-11-13 20:43:31 -0500

    Finished Round / Trick Testing is wip

---
+++ app/src/test/java/com/round/testRound.java
@@ -0,0 +1,17 @@
+package com.round;
+
+import com.Trick.Trick;
+import com.card.Card;
+import com.player.Player;
+import com.suits.Suit;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class testRound {
+    //assert.assertequals()
+    public void testDecideTrump(){
+
+    }
+}
---
+++ app/src/test/java/com/trick/testTrick.java
@@ -0,0 +1,4 @@
+package com.trick;
+
+public class testTrick {
+}
