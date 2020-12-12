1d3b8eee63056c11f3cdd64a36a9aabcedb4f9e6
nocturnalJack <70773502+nocturnalJack@users.noreply.github.com>
2020-12-11 20:13:25 -0500

    Implemented rules screen

--- app/src/main/java/com/example/tjeuchre/rules.java
+++ app/src/main/java/com/example/tjeuchre/rules.java
@@ -3,14 +3,24 @@ package com.example.tjeuchre;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class rules extends AppCompatActivity {
+    TextView textView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+        textView = (TextView) findViewById(R.id.textView2);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void mainActivity(View v)
